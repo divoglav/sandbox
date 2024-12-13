7444e793803114a8067544cc83cf52e00bdbeea1
divoglav <divoglav@protonmail.com>
2024-12-13 03:51:34 +0200

    feat: layers draw

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,11 @@
 import { onMount } from "solid-js";
-//import { Base } from "../../draws/archive/base/base";
-//import { Kernel } from "../../draws/archive/kernel/kernel";
-//import { Texture } from "../../draws/archive/texture/texture";
-//import { TextureCustom } from "../../draws/archive/texture-custom/texture-custom";
-//import { TextureCustom2 } from "../../draws/archive/texture-custom-2/texture-custom-2";
-import { TextureMultiple } from "../../draws/archive/texture-multiple/texture-multiple";
+import { Layers } from "../../draws/archive/layers/layers";
 
 export const Home = () => {
   let canvasRef!: HTMLCanvasElement;
 
   onMount(() => {
-    new TextureMultiple(canvasRef).setup();
+    new Layers(canvasRef).setup();
   });
 
   return (
